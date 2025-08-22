cb1370dd53452b17391617ab470b92c077de2725
danil <danil.chuprakov@avbinvest.co>
2025-08-22 15:49:11 +0300

    feature: Setting up classes to work with weather sites API

--- shared/src/androidMain/kotlin/com/project/nimbus/platform/PlatformModule.android.kt
+++ shared/src/androidMain/kotlin/com/project/nimbus/platform/PlatformModule.android.kt
@@ -2,7 +2,6 @@ package com.project.nimbus.platform
 
 import com.project.nimbus.domain.location.AndroidLocationService
 import com.project.nimbus.domain.location.LocationService
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule() = module {
---
+++ shared/src/iosMain/kotlin/com/project/nimbus/platform/ApiKeyProvider.ios.kt
@@ -0,0 +1,5 @@
+package com.project.nimbus.platform
+
+actual fun getApiKey(keyName: String): String {
+    TODO("Not yet implemented")
+}
